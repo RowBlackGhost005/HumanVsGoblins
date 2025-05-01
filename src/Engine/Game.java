5364cf3035f8dfcc1fd4e1fda0628340d1e975c9
Luis Angel Marin <la.marin@outlook.com>
2025-05-01 13:39:17 -0700

    N-S-W-E World gen movement added

--- src/Engine/Game.java
+++ src/Engine/Game.java
@@ -9,20 +9,66 @@ public class Game {
     public Game() {
         world = new WorldGen();
 
+        //Testing all cases
         moveNorth();
-        world.printWorld();
+        world.printNorth();
+        moveEast();
+        world.printEast();
+        moveSouth();
+        world.printSouth();
+        moveWest();
+        world.printWest();
     }
 
     public void moveNorth() {
-        //Ask to peak north
+        //Ask to peak
         Entity entity = world.peakNorth();
 
-        //Check if there is something on north
+        //Check if there is something
         if(entity != null){
             // Act accordingly
         }
 
         //If all good, move
         world.moveNorth();
+    }
+
+    public void moveEast() {
+        //Ask to peak
+        Entity entity = world.peakEast();
+
+        //Check if there is something
+        if(entity != null){
+            // Act accordingly
+        }
+
+        //If all good, move
+        world.moveEast();
+    }
+
+    public void moveSouth() {
+        //Ask to peak
+        Entity entity = world.peakSouth();
+
+        //Check if there is something
+        if(entity != null){
+            // Act accordingly
+        }
+
+        //If all good, move
+        world.moveSouth();
+    }
+
+    public void moveWest() {
+        //Ask to peak
+        Entity entity = world.peakWest();
+
+        //Check if there is something
+        if(entity != null){
+            // Act accordingly
+        }
+
+        //If all good, move
+        world.moveWest();
     }
 }
